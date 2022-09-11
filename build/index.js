3f47800a47feac4515d9c893e63b1460d1168c7d
L2HYUNN <dhl9810@naver.com>
2022-09-11 12:59:38 +0900

    #5.5 Blocks

--- build/index.js
+++ build/index.js
@@ -1,10 +1,2 @@
-var hello = function () { return "hi"; };
-var Block = /** @class */ (function () {
-    function Block(data) {
-        this.data = data;
-    }
-    Block.hello = function () {
-        return "hi";
-    };
-    return Block;
-}());
+"use strict";
+console.log("hi");
